3fab95361c197febe6c8ad221aeb9d637fba541a
evansamenorfe <94468805+evansamenorfe@users.noreply.github.com>
2023-06-29 10:26:49 +0000

    Delete chamber directory

--- chamber/scripts/hidden-time.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dateTimeField = document.getElementById('dateTime');
-    var currentDateTime = new Date().toISOString().slice(0, 16);
-    dateTimeField.value = currentDateTime;
-  });
