6b2aa1230d666edd4b0f988c8bc521e5d7c61dd7
evansamenorfe <evansamenorfe@gmail.com>
2023-07-17 21:37:46 +0000

    First and last lage done

--- bountiful-foods/scripts/display-side-images.js
+++ bountiful-foods/scripts/display-side-images.js
@@ -1,42 +1,39 @@
 var images = [
-    "images/fruit2.webp",
-    "images/fruit3.webp",
-    "images/fruit4.webp",
-    "images/fruit5.webp",
-    "images/fruit6.webp",
-    "images/fruit7.webp",
-    "images/fruit8.webp",
-    "images/fruit9.webp",
-    "images/fruit10.webp",
-    "images/fruit11.webp",
-    
-    
-  ];
+  "images/fruit2.webp",
+  "images/fruit3.webp",
+  "images/fruit4.webp",
+  "images/fruit5.webp",
+  "images/fruit6.webp",
+  "images/fruit7.webp",
+  "images/fruit8.webp",
+  "images/fruit9.webp",
+  "images/fruit10.webp",
+  "images/fruit11.webp",    
+];
   
-  var textOverlays = [,
-    "Nutritiuos fruits juice made from quality organic fruits",
-    "Deliveries available at your door step",
-    "Variaty of juice available on your order",
-    "How often do you take berries?",
-    "100% nutrients for your immune system",
-    "Ready for Cosumption",
-    "Fresh Organic Fruits",
-    "Fresh Organic Banana",
-    "Free Deliveries within Sanata Monica",
-    "Get yours now",
-    
-  ];
+var textOverlays = [
+  "Nutritiuos fruits juice made from quality organic fruits",
+  "Deliveries available at your door step",
+  "Variaty of juice available on your order",
+  "How often do you take berries?",
+  "100% nutrients for your immune system",
+  "Ready for Cosumption",
+  "Fresh Organic Fruits",
+  "Fresh Organic Produce",
+  "Free Deliveries within Sanata Monica",
+  "Get yours now",
+];
   
-  var currentImageIndex = 0;
-  var sidebarImage = document.getElementById("sidebar-image");
-  var overlayText = document.getElementById("overlay-text");
+var currentImageIndex = 0;
+var sidebarImage = document.getElementById("sidebar-image");
+var overlayText = document.getElementById("overlay-text");
   
-  function changeImage() {
-    sidebarImage.src = images[currentImageIndex];
-    overlayText.textContent = textOverlays[currentImageIndex];
-  
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-  }
-  
-  setInterval(changeImage, 10000); // Call changeImage every 10 seconds
+function changeImage() {
+  sidebarImage.src = images[currentImageIndex];
+  overlayText.textContent = textOverlays[currentImageIndex];
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 5000); // Call changeImage every 10 seconds
   
--- bountiful-foods/scripts/weather.js
+++ bountiful-foods/scripts/weather.js
@@ -1,22 +1,22 @@
 
-// select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const forecastContainer = document.querySelector('#forecast-container');
 
 const city = 'california';
 const units = 'imperial';
-const appId = 'eb9a0d74fedfaddac7cd755d3937ea78'; 
+const appId = 'eb9a0d74fedfaddac7cd755d3937ea78';
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${appId}`;
+const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${appId}`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${appId}`;
 
-async function apiFetch() {
+async function apiFetch(url) {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // this is for testing the call
-      displayResults(data);
+      return data;
     } else {
       throw Error(await response.text());
     }
@@ -25,30 +25,72 @@ async function apiFetch() {
   }
 }
 
-apiFetch();
-
-function calculateWindChill(temperature, windSpeed) {
-  const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-  return Math.round(windChill);
+async function displayWeather() {
+  try {
+    const currentWeatherData = await apiFetch(currentWeatherUrl);
+    const forecastData = await apiFetch(forecastUrl);
+    displayCurrentWeather(currentWeatherData);
+    displayForecast(forecastData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function displayResults(weatherData) {
+
+function displayCurrentWeather(weatherData) {
   currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  const windSpeed = weatherData.wind.speed;
-  const windChill = calculateWindChill(weatherData.main.temp, windSpeed);
+  const humidity = weatherData.main.humidity;
+  const iconCode = weatherData.weather[0].icon;
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
   const capitalizedDesc = desc.charAt(0).toUpperCase() + desc.slice(1).toLowerCase();
+  const weatherCode = weatherData.weather[0].icon;
+  const backgroundImageUrl = getBackgroundImageUrl(weatherCode);
 
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = capitalizedDesc;
-  document.getElementById('windSpeed').textContent = `Wind Speed: ${windSpeed} m/s`;
+  document.getElementById('humidity').textContent = `Humidity: ${humidity}%`;
+}
+function getBackgroundImageUrl(weatherCode) {
+  // Map weather code to corresponding image URL
+  const imageUrls = {
+    '01d': 'images/weather1.webp',
+    '01n': 'images/weather2.webp',
+    '02d': 'images/weather3.webp',
+    '02n': 'images/weather4.webp',
+    // Add more weather codes and corresponding image URLs as needed
+  };
+   // Default image URL for unknown weather codes
+   const defaultImageUrl = 'path/to/default-image.jpg';
 
-  const windChillElement = document.getElementById('windChill');
-  if (!isNaN(windChill)) {
-    windChillElement.textContent = `Wind Chill: ${windChill} °F`;
-  } else {
-    windChillElement.textContent = 'Wind Chill: N/A';
+   // Get the image URL based on the weather code
+   const imageUrl = imageUrls[weatherCode] || defaultImageUrl;
+ 
+   return imageUrl;
+ }
+
+function displayForecast(forecastData) {
+  forecastContainer.innerHTML = '<h2>Three-day Forecast</h2>';
+
+  for (let i = 0; i < 3; i++) {
+    const forecastItem = document.createElement('div');
+    forecastItem.classList.add('forecast-item');
+
+    const date = new Date(forecastData.list[i * 8].dt * 1000);
+    const temperature = forecastData.list[i * 8].main.temp.toFixed(0);
+    const iconCode = forecastData.list[i * 8].weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+    forecastItem.innerHTML = `
+      <p>Date: ${date.toDateString()}</p>
+      <p>Temperature: ${temperature}°F</p>
+      <img src="${iconUrl}" alt="Icon for weather">
+    `;
+
+    forecastContainer.appendChild(forecastItem);
   }
 }
+
+displayWeather();
+
